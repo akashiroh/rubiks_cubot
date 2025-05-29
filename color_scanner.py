8268032eb1bf7d2335c7635c309cb0178e0abaa2
Andrew <holmesa8@wwu.edu>
2025-05-29 11:02:28 -0700

    implemented opening webcam, scanning colors, and writing to a cube string, with wait time to deal with cube

--- color_scanner.py
+++ color_scanner.py
@@ -1,7 +1,10 @@
 import cv2 as cv
 import numpy as np
+import numpy.typing as npt
 from transform import color_to_pos
+import time
 
+# TODO: Needs to update based on orientation of the webcam compared to the cube in the robot housing
 face_order = {
     "U": [1, 2, 3, 4, 5, 6, 7, 8, 9],
     "R": [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -11,6 +14,7 @@ face_order = {
     "B": [3, 6, 9, 2, 5, 8, 1, 4, 7],
 }
 
+# TODO: Needs to update based on position of the webcam on the robot housing
 cell_to_pixel = {
     1: (190, 140),
     2: (320, 140),
@@ -23,12 +27,24 @@ cell_to_pixel = {
     9: (460, 380),
 }
 
-def determine_pixel_color(image, cell: int):
+def determine_pixel_color(
+    image: npt.NDArray[np.uint8], 
+    cell: int
+) -> str:
+    """
+    returns the pixel color for a cell on the cube face
+
+    Args:
+        image (ndarray): single frame from webcam
+        cell (int): [1..9] determines which cell (x, y) to scan
+    """
+
     x, y = cell_to_pixel[cell]
     b, g, r = image[x, y]
     h, s, v = cv.cvtColor(np.uint8([[[b, g, r]]]), cv.COLOR_BGR2HSV)[0][0]
 
     def classify_color(h, s, v):
+        """helper function to determine color based on HSV"""
         if v > 200 and s < 30:
             return 'W'
         if h < 10 or h > 160:
@@ -46,51 +62,59 @@ def determine_pixel_color(image, cell: int):
     color = classify_color(h, s, v)
     return color
 
-def scan_face(image, face: str):
+
+def open_webcam(
+    face: str,
+    debug: bool=True,
+) -> npt.NDArray[np.uint8]:
+    """
+    opens webcam and scans one face
+
+    Args: 
+        face (str): str in the set {U, D, F, B, L, R} to determine the order of the cells
+        debug (bool): keeps webcam open and draws points where it is scanning the color
+    Returns: frame (ndarray): single frame
+    """
+
+    webcam = cv.VideoCapture(0)
+    ret, frame = webcam.read()
+
+    webcam.release()
+    cv.destroyAllWindows()
+
+    return frame
+
+
+def scan_face(
+    face: str,
+    debug: bool=True,
+) -> str:
+    """
+    for each cell on the cube face, scan the color
+
+    Args:
+        image (ndarray): single frame from webcam
+        face (str): str in the set {U, D, F, B, L, R} to determine the order of the cells
+    """
+    image = open_webcam(face, debug)
+
     face_string = ""
     cell_order = face_order[face]
     for cell in cell_order:
         color = determine_pixel_color(image, cell)
         face_string += color
+
     return face_string
+
+
 
 if __name__ == "__main__":
 
-    webcam = cv.VideoCapture(0)
+    cube_string = ""
+    for face in ["U", "R", "F", "D", "L", "B"]:
+        face_string = scan_face(face)
+        cube_string += face_string
 
-    while True:
-        ret, frame = webcam.read()
-
-        width = int(webcam.get(cv.CAP_PROP_FRAME_WIDTH))
-        height = int(webcam.get(cv.CAP_PROP_FRAME_HEIGHT))
-
-        for i in range(1, 10):
-            point = cell_to_pixel[i]
-            face_image = cv.circle(frame, point, radius=5, color=(0, 255, 0), thickness=-1)
-
-        face_string = scan_face(frame, "U")
         print(face_string)
-        cv.imshow("Camera", frame)
-
-        if cv.waitKey(1) == ord("q"):
-            break
-    webcam.release()
-    cv.destroyAllWindows()
-
-
-    # cube_string = ""
-    # for face in ["U", "R", "F", "D", "L", "B"]:
-    #     face_image = cv.imread("rubiks_cube.jpg")
-
-    #     # for i in range(1, 10):
-    #     #     point = cell_to_pixel[i]
-    #     #     face_image = cv.circle(face_image, point, radius=5, color=(0, 255, 0), thickness=-1)
-    #     # cv.imshow("display", face_image)
-    #     # cv.waitKey(0)
-    #     # cv.destroyAllWindows()
-
-    #     face_string = scan_face(face_image, face)
-    #     cube_string += face_string
-    #     print(face_string)
-    # cube_string = "".join([color_to_pos[c] for c in cube_string])
-    # print(cube_string)
+        time.sleep(10)
+    print(cube_string)
